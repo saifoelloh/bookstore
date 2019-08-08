56922c98b298495d0bc12c910cd8fd80b0c1d42c
saifoelloh <saifoelloh@gmail.com>
2019-08-09 00:23:34 +0700

    updating result value of some method

--- src/book/controller.js
+++ src/book/controller.js
@@ -6,77 +6,31 @@ const Book = db.Book;
 module.exports = {
   index: async (req, res) => {
     try {
-      const { message, data } = await service.index();
+      const result = await service.index();
 
-      return response.success(res, {
-        code: 200,
-        message,
-        data,
-      });
+      return response.success(res, result);
     } catch (e) {
-      return response.failed(res, {
-        code: 500,
-        message,
-        data,
-      });
+      return response.failed(res, e);
     }
   },
   create: async (req, res) => {
-    const { data } = req.body;
-    const book = await Book.create(data)
-      .then(res => {
-        console.log('Horay.. successfully creating new book');
-        return res;
-      })
-      .catch(err => {
-        return response.failed(
-          res,
-          {
-            code: 500,
-            message: 'Failed creating new book',
-            data: null,
-          },
-          500,
-        );
-      });
+    try {
+      const { data } = req.body;
+      const result = await service.store(data);
 
-    return response.success(
-      res,
-      {
-        code: 201,
-        message: 'Success creating book',
-        data: book,
-      },
-      201,
-    );
+      return response.success(res, result);
+    } catch (e) {
+      return response.failed(res, e);
+    }
   },
   show: async (req, res) => {
-    const book = await Book.findByPk(req.params.id)
-      .then(res => {
-        console.log('Horay.. your data has been founded');
-        return res;
-      })
-      .catch(err => {
-        return response.failed(res, {
-          code: 500,
-          message: 'Internal server error',
-          data: e,
-        });
-      });
+    try {
+      const result = await service.show(req.params.id);
 
-    if (book === null) {
-      return response.failed(res, {
-        code: 404,
-        message: 'Your data not found',
-        data: null,
-      });
+      return response.success(res, result);
+    } catch (e) {
+      return response.failed(res, e);
     }
-
-    return response.success(res, {
-      code: 200,
-      message: 'Success getting detail of the book',
-      data: book,
-    });
   },
   update: async (req, res) => {
     const { data } = req.body;
