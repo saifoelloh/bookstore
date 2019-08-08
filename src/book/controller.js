452b15313d4e14d33cc1a3b25c4b6e635a4da68d
saifoelloh <saifoelloh@gmail.com>
2019-08-09 00:58:16 +0700

    finishing controller

--- src/book/controller.js
+++ src/book/controller.js
@@ -33,57 +33,22 @@ module.exports = {
     }
   },
   update: async (req, res) => {
-    const { data } = req.body;
+    try {
+      const { data } = req.body;
+      const result = await service.update(req.params.id, data);
 
-    const book = await Book.findByPk(req.params.id);
-    if (book === null) {
-      return response.failed(res, {
-        code: 404,
-        message: 'Data not found',
-        data: null,
-      });
+      return response.success(res, result);
+    } catch (e) {
+      return response.failed(res, e);
     }
-
-    const result = await book
-      .update(data)
-      .then(res => {
-        return res;
-      })
-      .catch(err => {
-        return response.failed(res, {
-          code: 500,
-          message: 'Internal server error',
-          data: null,
-        });
-      });
-
-    return response.success(res, {
-      code: 200,
-      message: 'Succesfully updating your data',
-      data: result,
-    });
   },
   destroy: async (req, res) => {
-    const result = await Book.destroy({
-      where: {
-        id: req.params.id,
-      },
-    })
-      .then(res => {
-        return res;
-      })
-      .catch(err => {
-        return response.failed(res, {
-          code: 500,
-          message: 'Internal server error',
-          data: null,
-        });
-      });
+    try {
+      const result = await service.destroy(req.params.id);
 
-    return response.success(res, {
-      code: 200,
-      message: 'Your data has been deleted',
-      data: result,
-    });
+      return response.success(res, result);
+    } catch (e) {
+      return response.failed(res, e);
+    }
   },
 };
